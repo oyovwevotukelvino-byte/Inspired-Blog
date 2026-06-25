ff70b73456bfa85e42b90b7a99bc484c4373e27a
Kelvin Oghenekevwe <kelvin@example.com>
2026-06-25 15:31:24 +0100

    Fix Post type for sermon fields

--- types/post.ts
+++ types/post.ts
@@ -4,15 +4,24 @@ export interface Post {
   _id: string
   title: string
   slug: { current: string }
+
   mainImage?: {
     asset: { _ref: string }
     crop?: any
     hotspot?: any
   }
+
   publishedAt: string
   categories: Category[]
   author: Author
   body: any[]
   excerpt?: string
-}
 
+  sermonVideo?: string
+
+  sermonAudio?: {
+    asset?: {
+      url?: string
+    }
+  }
+}
